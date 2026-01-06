cbb30146fc34e1d2b36214d732f42bcb3ca115d0
Hedayatullahnikzad <nikzad.hedayatullah@gmail.com>
2026-01-06 12:22:38 +0430

    build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1 className="text-[50px]">hello world</h1>
-    </>
+    <div className="grid grid-cols-[300px_1fr] grid-rows-[auto_1fr]">
+      <div className="bg-orange-400 col-span-2 p-2">Nav</div>
+      <div className="bg-yellow-400 p-2 hidden lg:block">Aside</div>
+      <div className="bg-blue-500 p-2 col-span-full lg:col-span-1 lg:col-start-2">
+        Main
+      </div>
+    </div>
   );
 }
 
