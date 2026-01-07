c558d9a6b5ae7b8f8f9029cec18ca5fa287e568e
Hedayatullahnikzad <nikzad.hedayatullah@gmail.com>
2026-01-07 11:47:11 +0430

    implement dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 
 function App() {
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
   return (
-    <div className="grid grid-cols-[300px_1fr] grid-rows-[auto_1fr]">
+    <div className="grid grid-cols-[300px_1fr] grid-rows-[auto_1fr] bg-white dark:bg-gray-900 text-black dark:text-white">
       <div className="col-span-2">
         <Navbar />
       </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,28 @@
 import logo from "../assets/logo.webp";
 const Navbar = () => {
+  const toggleTheme = () => {
+    const html = document.documentElement;
+    const isDark = html.classList.contains("dark");
+
+    if (isDark) {
+      html.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    } else {
+      html.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    }
+  };
   return (
-    <div className="flex items-center">
+    <div className="flex items-center dark:bg-gray-800">
       <img src={logo} alt="logo" className="w-[60px] h-[60px]" />
-      navbar
+      <span className="font-bold text-black dark:text-white">GameHub</span>
+
+      <button
+        onClick={toggleTheme}
+        className="px-3 py-1 rounded-md bg-black text-white dark:bg-white dark:text-black"
+      >
+        Toggle
+      </button>
     </div>
   );
 };
